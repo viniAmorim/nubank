bb21ae61b329943325ff5cdcd458ae1ab775ec03
viniAmorim <amorimm.viniciuss@gmail.com>
2021-08-21 18:48:16 -0300

    Tabs Components

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Header() {
-  return <Container />;
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+
+export default function Tabs() {
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#fff" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#fff" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#fff" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#fff" />
+          <TabText>Trasnferir</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="lock" size={24} color="#fff" />
+          <TabText>Bloquear cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
 }
